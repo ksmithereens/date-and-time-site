2422cbf086e8d0e20b37560398143d3e07c6446c
Khaleel Smith <145707575+ksmithereens@users.noreply.github.com>
2024-03-25 05:31:08 +0000

    Temperature Added

--- js/script.js
+++ js/script.js
@@ -12,4 +12,35 @@ lightDark.addEventListener( 'click', () => {
   //   Check check
   console.log("OK?");
   
+})
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+// const city = 'miami';
+const zip = 99202;
+const APIKEY = 'f2f85762267ef18d0df71e18ec9f3737';
+const units = 'imperial';
+
+// the data
+const dataSource = `${url}?zip=${zip}&appid=${APIKEY}&units=${units}`;
+
+// fetch it
+fetch(dataSource)
+  .then( response => response.json() )
+  .then( myData => {
+  
+  // get stuff
+  const body = document.querySelector('body');
+  const h1 = document.querySelector('h1');
+  const h2 = document.querySelector('h2');
+ 
+  // template
+  const template = `
+      <h1> ${myData.main.temp} </h1>
+      <h2> ${myData.name} </h2>
+  `
+  
+  // display template
+  body.insertAdjacentHTML( "afterbegin", template );
+  
+  
 });
